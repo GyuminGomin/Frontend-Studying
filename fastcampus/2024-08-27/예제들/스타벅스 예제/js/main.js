e1ad9fb38691d178a99f4cc144f89a3da66e4d23
gyumingomin <ool9898@naver.com>
2024-11-21 22:47:29 +0900

    스타벅스 완료

--- fastcampus/2024-08-27/예제들/스타벅스 예제/js/main.js
+++ fastcampus/2024-08-27/예제들/스타벅스 예제/js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -26,6 +27,10 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
+    });
+    // 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
     })
   } else {
     // 배지 나타남
@@ -33,8 +38,18 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 1,
       display: 'block'
     })
+    // 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
 }, 300));
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
